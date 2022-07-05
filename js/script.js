7b0d2a572cd20c3af1375a7410962a6f435c1dd1
antaninazhuk <101988172+antaninazhuk@users.noreply.github.com>
2022-07-05 22:07:56 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Task 1
+
+// const button = document.querySelector('.button_push');
+
+// первый способ. прописан в HTML
+
+// второй способ:
+// button.addEventListener("click", function(e) {
+//     console.log('Спасибо, что нажал!');
+// });
+
+
+// третий способ:
+// button.onclick = function() {
+//     console.log("Еще раз спасибо");
+// };
+
+
+
+// Task 2
+// const button = document.querySelector('.button_dobble_push');
+
+// button.addEventListener("dblclick", function(e) {
+//     console.log('Ты нажал дважды');
+// });
+
+
+
+
+// Task 3
+
+// const field = document.querySelector('.field');
+// field.addEventListener('mousemove', function(e) {
+//     console.log(event.clientX, event.clientY);
+// })
+
+
+
+// Task 4
+
+// window.addEventListener('scroll', function(event) {
+//     console.log(`${scrollY}px and ${scrollX}px`);
+// });
